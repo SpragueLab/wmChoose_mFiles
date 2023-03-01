1b925762fe9d23e1016be0a273c94cd39c144f27
Alison Li <68033091+alison-yanming-li@users.noreply.github.com>
2023-03-01 11:20:18 -0800

    03012023CurveFitting4AllSub

--- DoG.m
+++ DoG.m
@@ -83,6 +83,26 @@ all_data.use_trial = ~cellfun( @any, cellfun( @(a) ismember(a, WHICH_EXCL), all_
 % drop trials with very short (< 100 ms) or very long RT (> 1 s)
 all_data.use_trial(all_data.s_all.i_sacc_rt<0.1 | all_data.s_all.i_sacc_rt>1.5) = 0;
 
+
+%% table of % included or excluded trials from each subj
+
+percent_useTrial = nan(length(u_subj),1);
+percent_excTrial = nan(length(u_subj),1);
+num_useTrial = nan(length(u_subj),1);
+
+
+for ss = 1:length(u_subj)
+
+    thisidx = all_data.subj_all==ss & all_data.use_trial==1;
+    thisidx2 = all_data.subj_all==ss & all_data.use_trial==0;
+    thisidx3 = all_data.subj_all==ss;
+    percent_useTrial(ss) = sum(thisidx)/sum(thisidx3);
+    percent_excTrial(ss) = sum(thisidx2)/sum(thisidx3);
+    num_useTrial(ss) = sum(thisidx);
+
+end
+
+
 %% Prep variables
 
 %convert all xy coordinate to angle
@@ -116,8 +136,8 @@ all_data.s_all.targAng180 = atan2d(all_data.s_all.targ(:,2),all_data.s_all.targ(
 all_data.s_all.f_sacc_RawAng = atan2d(all_data.s_all.f_sacc_raw(:,2),all_data.s_all.f_sacc_raw(:,1));
 all_data.s_all.f_sacc_align180 = atan2d(all_data.s_all.f_sacc(:,2),all_data.s_all.f_sacc(:,1)); %all target is align to one point like 0 degree, i.e., reported loc - target
 
-
 targ_diff = nan(length(all_data.t_all),1);
+% targ_diff2 = nan(length(all_data.t_all) - length(all_data.r_all),1);
 
 for ss = 1:length(u_subj)
 
@@ -128,8 +148,8 @@ for ss = 1:length(u_subj)
     for rr = 1:length(ru)
         for tt = 2:length(tu)
 
-            thisidx1 = all_data.subj_all==ss & all_data.r_all == rr & all_data.t_all == tt;
-            thisidx2 = all_data.subj_all==ss & all_data.r_all == rr & all_data.t_all == tt-1;
+            thisidx1 = all_data.subj_all==ss & all_data.r_all == ru(rr) & all_data.t_all == tt; %without ru or tu sometimes cause error with consecutive
+            thisidx2 = all_data.subj_all==ss & all_data.r_all == ru(rr) & all_data.t_all == tt-1;
 
             targ_diff(thisidx1) = angdiffdeg(all_data.s_all.targAng180(thisidx1),all_data.s_all.targAng180(thisidx2)); %current trial ang - previous trial ang
 
@@ -137,12 +157,15 @@ for ss = 1:length(u_subj)
     end
 end
 all_data.use_trial(all_data.t_all == 1) = 0;
+% targ_diff2 = targ_diff(all_data.use_trial == 1);
+
+
 %% plot targdiff against error
 
 for ss= 1:length(u_subj)
     subplot(1,length(u_subj),ss); hold on
 %     plot(targ_diff(all_data.subj_all == ss), all_data.s_all.f_sacc_align180(all_data.subj_all == ss),'ko','MarkerFaceColor','k','');
-    scatter(targ_diff(all_data.subj_all == ss), all_data.s_all.f_sacc_align180(all_data.subj_all == ss),10,'k','filled','MarkerFaceAlpha',0.25);%MarkerFaceColor','k','');
+    scatter(targ_diff(all_data.subj_all == ss & all_data.use_trial==1), all_data.s_all.f_sacc_align180(all_data.subj_all == ss & all_data.use_trial==1),10,'k','filled','MarkerFaceAlpha',0.25);%MarkerFaceColor','k','');
     if ss ==1
     xlabel('target difference between current trial and previous trial in angle')
     ylabel('final saccade error with algined target location')
@@ -157,35 +180,45 @@ end
 
 
 
-%%
-this_subj = 1;
+%% plot individual sub data
+this_subj = 19;
 figure;
- scatter(targ_diff(all_data.subj_all == this_subj), all_data.s_all.f_sacc_align180(all_data.subj_all == this_subj),10,'k','filled','MarkerFaceAlpha',0.25);%MarkerFaceColor','k','');
+ scatter(targ_diff(all_data.subj_all == this_subj & all_data.use_trial==1), all_data.s_all.f_sacc_align180(all_data.subj_all == this_subj & all_data.use_trial==1),10,'k','filled','MarkerFaceAlpha',0.25);%MarkerFaceColor','k','');
 
 hold on
 
 c = sqrt(2)/exp(-0.5);
-a = 15;
-w = 1/15;
+a = 3;
+w = 0.015;
 
 x = [-180:1:180];
 
 y = x.*a.*w.*c.*exp(-(w.*x).^2);
 
-%plot(x,y,'r-');
+xlim([-180 180]);ylim([-10 10]);
+
+plot(x,y,'r-');
 
 %% manual error measurement
-my_x = targ_diff(all_data.subj_all == this_subj & all_data.use_trial==1);
-my_y = all_data.s_all.f_sacc_align180(all_data.subj_all == this_subj & all_data.use_trial==1);
+
+opt_param = nan(length(u_subj),2);
+opt_ssRes = nan(length(u_subj),1);
+opt_ve = nan(length(u_subj),1);
+
+for ss = 1:length(u_subj)
+ subplot(3,7,ss); hold on
+% this_subj = 19;
+my_x = targ_diff(all_data.subj_all == ss & all_data.use_trial==1);
+my_y = all_data.s_all.f_sacc_align180(all_data.subj_all == ss & all_data.use_trial==1);
 
 fun_x = my_x;
-fun_y = fun_x.*a.*w.*c.*exp(-(w.*fun_x).^2);
-
-plot (fun_x,fun_y,'b.');
-
-error_mea= my_y - fun_y;
-square_error = error_mea.^2;
-sum_error = sum(square_error);
+% fun_y = fun_x.*a.*w.*c.*exp(-(w.*fun_x).^2);
+plot (my_x,my_y,'r.');hold on
+% % plot (fun_x,fun_y,'b.');
+% 
+% error_mea= my_y - fun_y;
+% square_error = error_mea.^2;
+% sum_error = sum(square_error);
 
 % w is p(1); a is p(2)
 myerrfcn = @(p) sum( (  my_y -  (fun_x.*p(2).*p(1).*c.*exp(-(p(1).*fun_x).^2)) ).^2  ) ; % computes SSE
@@ -193,17 +226,41 @@ myerrfcn = @(p) sum( (  my_y -  (fun_x.*p(2).*p(1).*c.*exp(-(p(1).*fun_x).^2)) )
 options = optimset('fmincon');
 options.Display = 'off';
 
-init_guess = [1/15 10]; 
+% init_guess = [1/15 10];  
+init_guess = [0.015 3]; 
+
 [bf_params, bf_err, ex_flag] = fmincon( myerrfcn, init_guess, [], [], [], [], [0 -inf], [inf inf], [], options );
 %[bf_params, bf_err, ex_flag] = fmincon( myerrfcn, init_guess);
 
+opt_param(ss,:) = [bf_params(1), bf_params(2)]; 
+opt_ssRes(ss) = [bf_err];
+opt_ve(ss) = [1-opt_ssRes(ss)/sum((my_y-mean(my_y)).^2)];
+
 c = sqrt(2)/exp(-0.5);
-a = bf_params(1);
-w = bf_params(2);
+a = bf_params(2);
+w = bf_params(1);
 
 x = [-180:1:180];
 
 y = x.*a.*w.*c.*exp(-(w.*x).^2);
 
-
+xlim([-180 180]);ylim([-10 10]);
 plot(x,y,'-');
+
+clear a w y bf_params;
+
+end
+
+%% parameter fit
+
+good_sub = opt_ve >= 0;% excluded two subj (16 and 17) because the model fit explain the variance less than the data itself
+
+figure;
+
+subplot(1,2,1);hold on
+
+plot(0, opt_param(good_sub,1),'ko');
+subplot(1,2,2);
+plot(1, opt_param(good_sub,2),'ro')
+figure;
+scatter(opt_param(good_sub,1),opt_param(good_sub,2))
